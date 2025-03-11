9c7947c901cd0e84dc77dc429f73251be887672a
Shahriyor <baxtiyarovich0102@gmail.com>
2025-03-11 17:06:53 +0500

    uploads qismidan tashqari barchasi yaratildi

--- middlewares/app.js
+++ middlewares/app.js
@@ -2,9 +2,11 @@ const express = require("express")
 let app = express()
 
 let postRouter = require("../routes/post.route")
+let userRouter = require("../routes/user.route")
 
 app.use(express.json())
-app.use("/api/posts", postRouter)
+app.use("/posts", postRouter)
+app.use("/users", userRouter)
 
 
 app.get("*", (req, res, next) => {
